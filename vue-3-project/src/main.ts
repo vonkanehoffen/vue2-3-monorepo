a9920d692258a69957810511715a138194315b65
Kane Clover <kane.clover@gmail.com>
2022-11-28 14:22:37 +0000

    remove vue-final-modal

--- vue-3-project/src/main.ts
+++ vue-3-project/src/main.ts
@@ -1,11 +1,8 @@
 import { createApp } from 'vue'
-import { vfmPlugin } from 'vue-final-modal'
-
 import App from './App.vue'
 
 import './assets/main.css'
 
 const app = createApp(App);
-app.use(vfmPlugin);
 
 app.mount('#app')
